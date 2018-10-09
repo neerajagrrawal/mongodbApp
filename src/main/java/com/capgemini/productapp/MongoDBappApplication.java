88746c85707d27fc4eb69a2e84525ac1b9b96227
AGRAWAL <neeraj.agrawal@capgemini.com>
2018-10-09 02:19:02 -0400

    first commit

---
+++ src/main/java/com/capgemini/productapp/MongoDBappApplication.java
@@ -0,0 +1,12 @@
+package com.capgemini.productapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDBappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoDBappApplication.class, args);
+	}
+}
